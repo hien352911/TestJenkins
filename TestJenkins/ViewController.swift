4f0b54af2ff11b06290a9637792a27cb7092efa0
HienBD <hienbd@techcombank.com.vn>
2021-09-12 20:53:36 +0700

    init commit

---
+++ TestJenkins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestJenkins
+//
+//  Created by Hien IT. Bui Duy on 12/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let temp = infoForKey("Backend Url")
+        print(temp)
+    }
+
+
+}
+
+func infoForKey(_ key: String) -> String? {
+    return (Bundle.main.infoDictionary?[key] as? String)?
+        .replacingOccurrences(of: "\\", with: "")
+}
